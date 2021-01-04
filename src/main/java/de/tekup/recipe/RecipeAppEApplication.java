bd90638da60cc899f0d220fffb367bd7f34c4caf
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-01-04 14:12:13 +0100

    empty project

---
+++ src/main/java/de/tekup/recipe/RecipeAppEApplication.java
@@ -0,0 +1,13 @@
+package de.tekup.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeAppEApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/tekup/recipe/RecipeAppEApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tekup.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
